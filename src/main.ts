26e38a4d0670c5c1fd43cf1f907474392b846f3b
Andrew S Erwin <andrewerwin73@gmail.com>
2025-08-08 17:30:25 -0500

    refactor: move stylesheet to public directory and import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Game } from './Game.ts';
+import '/public/styles/styles.css';
 
 const canvas = document.getElementById("gamearena") as HTMLCanvasElement;
 const bpcanvas = document.getElementById("bullpen") as HTMLCanvasElement;
